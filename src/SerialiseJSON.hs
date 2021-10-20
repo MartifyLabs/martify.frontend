4ad4686d5fcdbec152560ae3f9bc5cde8bccbd6a
Alain Magazin <alain.magazin@outlook.com>
2021-10-20 17:42:58 +0200

    Validator V0.2

--- src/SerialiseJSON.hs
+++ src/SerialiseJSON.hs
@@ -7,7 +7,6 @@ module SerialiseJSON (testR, testD) where
 import Data.Aeson as Json ( encode )
 import Data.ByteString.Lazy qualified as LB
 
-
 import Cardano.Api
     ( scriptDataToJson,
       ScriptDataJsonSchema(ScriptDataJsonDetailedSchema) )
@@ -16,6 +15,7 @@ import qualified PlutusTx
 
 import Market.Types (SaleAction(..), NFTSale(..))
 
+
 -- This module is here to convert Haskell Data Types to JSON files, particularly used for Redeemer and Datum
 -- To use this enter `cabal repl` ; `:l src/SerialiseJSON.hs` ; `testR` or `testD`
 
@@ -23,18 +23,16 @@ import Market.Types (SaleAction(..), NFTSale(..))
 -- Constructs the JSON file for the Buy Redeemer constructor, used as input to --tx-in-redeemer-file
 testR :: IO ()
 testR = do 
-  writeData "update.json" Update
+  writeData "redeemer.json" Buy
   putStrLn "Done"
 
 
 nftEx :: NFTSale
 nftEx = NFTSale
-    { nPrice    = 1
-    , nSeller   = "4b4355acf5e549d4cd26408723d1c8d87a98db7598e40bb1cd3b343e"
-    , nToken    = "Vendere"
-    , nCurrency = "8b050684e8d7e1abb2b79227f44c4c767025decb140e64efc783d046"
-    , nRoyAddr  = nSeller nftEx
-    , nRoyPrct  = 0
+    { nPrice    = 10_000_000
+    , nSeller   = ""
+    , nToken    = "Vendere2"
+    , nCurrency = "66"
     } -- This is an example to fill with real data
       -- The `nSeller` needs to be in Base16 format, not Bech32 (addr1...).
       -- To easily get the Base16 version, go to Cardanoscan.io, search the address in format addr1...
