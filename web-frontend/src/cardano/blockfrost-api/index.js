369589c3b4b54488756b385952349e92c7bb9844
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2021-10-25 12:49:03 +0100

    define blockfrost api

---
+++ web-frontend/src/cardano/blockfrost-api/index.js
@@ -0,0 +1,20 @@
+import { apiKey, cardanoUrl, ipfsUrl } from '../../config';
+
+export const cardanoBlockfrost = async (endpoint, headers, body) => {
+  return await request(cardanoUrl, endpoint, headers, body);
+};
+
+export const ipfsBlockfrost = async (endpoint, headers, body) => {
+  return await request(ipfsUrl, endpoint, headers, body);
+};
+
+const request = async (base, endpoint, headers, body) => {
+  return await fetch(base + endpoint, {
+    headers: {
+      project_id: apiKey,
+      ...headers,
+    },
+    method: body ? "POST" : "GET",
+    body,
+  }).then((res) => res.json());
+};
--- web-frontend/src/config.js
+++ web-frontend/src/config.js
@@ -1,6 +1,12 @@
+// TODO: store configuration in firebase
+
 /* production */
 
 
 /* development */
 export const api_host = "http://localhost:4000";
 export const cardanoscan_url = "https://testnet.cardanoscan.io/transaction/";
+// Blockfrost
+export const apiKey = "testnetAWfuCqaLZ5KtOSslc8ZTZyEWj36We8h7";
+export const cardanoUrl = "https://cardano-testnet.blockfrost.io/api/v0/";
+export const ipfsUrl = "https://cardano-testnet.blockfrost.io/api/v0/";
