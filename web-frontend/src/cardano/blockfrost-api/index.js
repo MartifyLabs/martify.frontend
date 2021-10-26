4dc0bc3e67cb016d437b6af85a668b9628e65d39
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2021-10-26 10:28:26 +0100

    query protocol parameters using blockfrost

--- web-frontend/src/cardano/blockfrost-api/index.js
+++ web-frontend/src/cardano/blockfrost-api/index.js
@@ -1,5 +1,23 @@
 import { apiKey, cardanoUrl, ipfsUrl } from "../../config";
 
+export const getProtocolParameters = async () => {
+  const response = await cardanoBlockfrost(`/epochs/latest/parameters`);
+  
+  return {
+    linearFee: {
+      minFeeA: response.min_fee_a.toString(),
+      minFeeB: response.min_fee_b.toString(),
+    },
+    minUtxo: response.min_utxo,
+    poolDeposit: response.pool_deposit,
+    keyDeposit: response.key_deposit,
+    maxValSize: parseInt(response.max_val_size),
+    maxTxSize: parseInt(response.max_tx_size),
+    priceMem: parseFloat(response.price_mem),
+    priceStep: parseFloat(response.price_step),
+  };
+};
+
 export const cardanoBlockfrost = async (endpoint, headers, body) => {
   return await request(cardanoUrl, endpoint, headers, body);
 };
