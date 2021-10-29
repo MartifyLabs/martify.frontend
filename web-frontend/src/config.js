6590a71197dea23f8b2c767247e0c561c252fe4f
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2021-10-29 16:58:28 +0100

    enhance the blockfrost api

--- web-frontend/src/config.js
+++ web-frontend/src/config.js
@@ -9,4 +9,4 @@ export const cardanoscan_url = "https://testnet.cardanoscan.io/transaction/";
 // Blockfrost
 export const apiKey = "testnetAWfuCqaLZ5KtOSslc8ZTZyEWj36We8h7";
 export const cardanoUrl = "https://cardano-testnet.blockfrost.io/api/v0/";
-export const ipfsUrl = "https://cardano-testnet.blockfrost.io/api/v0/";
+export const ipfsUrl = "https://ipfs.blockfrost.io/api/v0/";
