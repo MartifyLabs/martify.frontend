4837834ae8313cff4e38ee29d3c3fa61c0db253b
Hong Jing (Jingles) <jinglescode@gmail.com>
2021-11-01 23:22:49 +0800

    from jingles, data schema, and many work in progress

--- web-frontend/src/store/collection/api.js
+++ web-frontend/src/store/collection/api.js
@@ -31,7 +31,8 @@ export const get_listings = (policy_id, callback) => async (dispatch) => {
     "policy_id": policy_id,
     "listing": data_assets[policy_id]
   };
-
+  console.log(22, policy_id)
+  console.log(11, data_assets)
   if(output.policy_id && output.listing){
     dispatch(collections_add_tokens(output));
   }
--- web-frontend/src/store/collection/collectionReducer.js
+++ web-frontend/src/store/collection/collectionReducer.js
@@ -9,9 +9,15 @@ let collectionobj = {
   policies_tokens: {},
 };
 
-function update_tokens(token, asset_id, policy_id){
-  token.id = asset_id;
-  token.policy_id = policy_id;
+function update_tokens(token, asset_id, policy_id, state_policies_collections){
+  // token.id = asset_id;
+  // token.policy_id = policy_id;
+  console.log(policy_id, state_policies_collections)
+  if(policy_id in state_policies_collections){
+    token.collection = state_policies_collections[policy_id];
+  }else{
+    token.collection = {policy_id: policy_id};
+  }
   return token;
 }
 
@@ -20,22 +26,26 @@ export default function collectionReducer(state = collectionobj, { type, payload
 
     case types.COLLECTIONS_LOADED:
       var tmp_policies_collections = {...state.policies_collections};
+      var tmp_collections = {...state.collections};
+
       for(var collection_id in payload){
-        tmp_policies_collections[payload[collection_id].policy_id] = collection_id;
+        tmp_collections[collection_id] = payload[collection_id].policy_id
+        tmp_policies_collections[payload[collection_id].policy_id] = payload[collection_id];
       }
       return {
         ...state,
         loaded: true,
-        collections: payload,
+        collections: tmp_collections,
         policies_collections: tmp_policies_collections
       };
+      
     case types.COLLECTIONS_LOADING:
       return {
         ...state,
         loading: payload
       };
     case types.COLLECTIONS_LISTING_LOADED:
-      let tmp_collections = {
+      var tmp_collections = {
         ...state.collections,
       };
       tmp_collections[payload.collection_id].listing = payload.listing;
@@ -46,14 +56,14 @@ export default function collectionReducer(state = collectionobj, { type, payload
       };
     
      case types.COLLECTIONS_ADD_TOKENS:
-      let tmp = {
+      var tmp = {
         ...state.policies_tokens,
       };
       
-      let new_tokens = {...payload.listing};
+      var new_tokens = {...payload.listing};
 
       for(var asset_id in new_tokens){
-        new_tokens[asset_id] = update_tokens(new_tokens[asset_id], asset_id, payload.policy_id);
+        new_tokens[asset_id] = update_tokens(new_tokens[asset_id], asset_id, payload.policy_id, state.policies_collections);
       }
       if(!(payload.policy_id in tmp)){
         tmp[payload.policy_id] = {};
@@ -71,20 +81,39 @@ export default function collectionReducer(state = collectionobj, { type, payload
 
 
     case walletTypes.SET_WALLET_ASSETS:
-      let tmp_policies_tokens = {...state.policies_tokens};
+      var tmp_policies_tokens = {...state.policies_tokens};
 
-      for(var policy_id in payload){
-        var tmp_policies_tokens_policy = {};
-        if(policy_id in tmp_policies_tokens){
-          tmp_policies_tokens_policy = {...tmp_policies_tokens[policy_id]};
-        }
+      for(var asset_id in payload){
+        if(asset_id!='lovelace'){
+          var this_asset = payload[asset_id];
+          var policy_id = this_asset.info.policyId;
 
-        for(var asset_id in payload[policy_id]){
-          tmp_policies_tokens_policy[asset_id] = payload[policy_id][asset_id];
+          var tmp_policies_tokens_policy = {};
+          if(policy_id in tmp_policies_tokens){
+            tmp_policies_tokens_policy = {...tmp_policies_tokens[policy_id]};
+          }
+
+          tmp_policies_tokens_policy[asset_id] = {
+            info: this_asset.info,
+            listing: {}
+          }
+
+          tmp_policies_tokens[policy_id] = tmp_policies_tokens_policy;
         }
-        
-        tmp_policies_tokens[policy_id] = tmp_policies_tokens_policy;
       }
+
+      // for(var policy_id in payload){
+      //   var tmp_policies_tokens_policy = {};
+      //   if(policy_id in tmp_policies_tokens){
+      //     tmp_policies_tokens_policy = {...tmp_policies_tokens[policy_id]};
+      //   }
+
+      //   for(var asset_id in payload[policy_id]){
+      //     tmp_policies_tokens_policy[asset_id] = payload[policy_id][asset_id];
+      //   }
+        
+      //   tmp_policies_tokens[policy_id] = tmp_policies_tokens_policy;
+      // }
       
       return {
         ...state,
