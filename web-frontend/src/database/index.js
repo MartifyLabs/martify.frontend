36064db2286909b7977b329f34e91baed6941d75
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2021-11-03 11:30:57 +0100

    initial implementation for firebase storage

--- web-frontend/src/database/index.js
+++ web-frontend/src/database/index.js
@@ -1,15 +1,12 @@
 import { initializeApp } from "firebase/app";
 import {
+  addDoc,
   collection,
-  doc,
   getDocs,
   getFirestore,
   query,
-  setDoc,
   where,
-  limit,
 } from "firebase/firestore";
-import { getAssetInfo, getMintedAssets } from "../cardano/blockfrost-api";
 import { firebaseOptions } from "../config";
 
 const app = initializeApp(firebaseOptions);
@@ -26,15 +23,9 @@ export const getAssets = async (policyId) => {
     const snapshot = await getDocs(result);
 
     if (snapshot.empty) {
-      const assetIds = await getMintedAssets(policyId);
-      const assets = await Promise.all(
-        assetIds.map(async (assetId) => await getAssetInfo(assetId))
+      console.log(
+        `No Assets found for policyId: ${policyId} in firebase, will be querying blockfrost instead... Work In Progress...`
       );
-
-      await saveAssets(assets);
-      return assets.map((asset) => {
-        return { info: asset };
-      });
     } else {
       return snapshot.docs.map((doc) => doc.data());
     }
@@ -43,21 +34,20 @@ export const getAssets = async (policyId) => {
   }
 };
 
-export const getAsset = async (assetId) => {
+export const getAsset = async (policyId, assetId) => {
   try {
     const result = await query(
       collection(db, "assets"),
+      where("info.policyId", "==", policyId),
       where("info.asset", "==", assetId)
     );
 
     const snapshot = await getDocs(result);
 
     if (snapshot.empty) {
-      const asset = await getAssetInfo(assetId)
-
-      await saveAssets([asset]);
-
-      return { info: asset };
+      console.log(
+        `No Asset found with id: ${assetId} in firebase, will be querying blockfrost instead... Work In Progress...`
+      );
     } else {
       return snapshot.docs[0].data();
     }
@@ -70,50 +60,12 @@ export const saveAssets = async (assets) => {
   try {
     await Promise.all(
       assets.map(async (asset) => {
-        if (asset && asset.onchainMetadata) {
-          await setDoc(doc(db, "assets", asset.asset), {
-            info: asset,
-          }, { merge: true });
-        }
+        await addDoc(collection(db, "assets"), asset);
       })
     );
   } catch (error) {
     console.error(
       `Unexpected error in saveAssets. [Message: ${error.message}]`
-    );
-  }
-};
-
-export const saveAsset = async (asset) => {
-  try {
-    if (asset) {
-      await setDoc(doc(db, "assets", asset.info.asset), {
-        ...asset
-      });
-    }
-  } catch (error) {
-    console.error(
-      `Unexpected error in saveAsset. [Message: ${error.message}]`
-    );
-  }
-};
-
-
-export const getRandomAssets = async (count) => {
-  try {
-    
-    const result = await query(
-      collection(db, "assets"),
-      where("info.policyId", "!=", null),
-      limit(count)
-    );
-
-    const snapshot = await getDocs(result);
-    return snapshot.docs.map((doc) => doc.data());
-
-  } catch (error) {
-    console.error(
-      `Unexpected error in saveAsset. [Message: ${error.message}]`
     );
   }
 };
