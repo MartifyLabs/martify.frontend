1a80b4bfc193bd43da8b57073db1b237e9004aac
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2021-11-03 11:30:57 +0100

    initial implementation for firebase storage

--- web-frontend/src/config.js
+++ web-frontend/src/config.js
@@ -2,7 +2,6 @@
 
 /* production */
 
-
 /* development */
 export const api_host = "http://localhost:4000";
 export const cardanoscan_url = "https://testnet.cardanoscan.io/transaction/";
@@ -10,3 +9,11 @@ export const cardanoscan_url = "https://testnet.cardanoscan.io/transaction/";
 export const apiKey = "testnetAWfuCqaLZ5KtOSslc8ZTZyEWj36We8h7";
 export const cardanoUrl = "https://cardano-testnet.blockfrost.io/api/v0/";
 export const ipfsUrl = "https://ipfs.blockfrost.io/api/v0/";
+// Firebase
+export const firebaseOptions = {
+  apiKey: "AIza....", // Auth / General Use
+  authDomain: "YOUR_APP.firebaseapp.com", // Auth with popup/redirect
+  databaseURL: "https://YOUR_APP.firebaseio.com", // Realtime Database
+  storageBucket: "YOUR_APP.appspot.com", // Storage
+  messagingSenderId: "123456789", // Cloud Messaging
+};
---
+++ web-frontend/src/database/index.js
@@ -0,0 +1,71 @@
+import { initializeApp } from "firebase/app";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
+import { firebaseOptions } from "../config";
+
+const app = initializeApp(firebaseOptions);
+
+const db = getFirestore(app);
+
+export const getAssets = async (policyId) => {
+  try {
+    const result = await query(
+      collection(db, "assets"),
+      where("info.policyId", "==", policyId)
+    );
+
+    const snapshot = await getDocs(result);
+
+    if (snapshot.empty) {
+      console.log(
+        `No Assets found for policyId: ${policyId} in firebase, will be querying blockfrost instead... Work In Progress...`
+      );
+    } else {
+      return snapshot.docs.map((doc) => doc.data());
+    }
+  } catch (error) {
+    console.error(`Unexpected error in getAssets. [Message: ${error.message}]`);
+  }
+};
+
+export const getAsset = async (policyId, assetId) => {
+  try {
+    const result = await query(
+      collection(db, "assets"),
+      where("info.policyId", "==", policyId),
+      where("info.asset", "==", assetId)
+    );
+
+    const snapshot = await getDocs(result);
+
+    if (snapshot.empty) {
+      console.log(
+        `No Asset found with id: ${assetId} in firebase, will be querying blockfrost instead... Work In Progress...`
+      );
+    } else {
+      return snapshot.docs[0].data();
+    }
+  } catch (error) {
+    console.error(`Unexpected error in getAsset. [Message: ${error.message}]`);
+  }
+};
+
+export const saveAssets = async (assets) => {
+  try {
+    await Promise.all(
+      assets.map(async (asset) => {
+        await addDoc(collection(db, "assets"), asset);
+      })
+    );
+  } catch (error) {
+    console.error(
+      `Unexpected error in saveAssets. [Message: ${error.message}]`
+    );
+  }
+};
