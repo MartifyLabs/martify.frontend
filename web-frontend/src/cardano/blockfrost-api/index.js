893c1a0c17760a84d95a57c7a82e8e128dc43aa1
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2021-11-04 13:55:49 +0100

    get minted assets by a policy id

--- web-frontend/src/cardano/blockfrost-api/index.js
+++ web-frontend/src/cardano/blockfrost-api/index.js
@@ -4,47 +4,77 @@ import { apiKey, cardanoUrl } from "../../config";
  * @param {string} asset - asset is a Concatenation of the policy_id and hex-encoded asset_name.
  */
 export const getAssetInfo = async (asset) => {
-  const response = await cardano(`assets/${asset}`);
+  try {
+    const response = await cardano(`assets/${asset}`);
 
-  return {
-    asset: response.asset,
-    policyId: response.policy_id,
-    assetName: response.asset_name,
-    fingerprint: response.fingerprint,
-    quantity: parseInt(response.quantity),
-    initialMintTxHash: response.initial_mint_tx_hash,
-    mintOrBurnCount: parseInt(response.mint_or_burn_count),
-    onchainMetadata: response.onchain_metadata,
-    metadata: response.metadata,
-  };
+    return {
+      asset: response.asset,
+      policyId: response.policy_id,
+      assetName: response.asset_name,
+      fingerprint: response.fingerprint,
+      quantity: parseInt(response.quantity),
+      initialMintTxHash: response.initial_mint_tx_hash,
+      mintOrBurnCount: parseInt(response.mint_or_burn_count),
+      onchainMetadata: response.onchain_metadata,
+      metadata: response.metadata,
+    };
+  } catch (error) {
+    console.error(
+      `Unexpected error in getAssetInfo. [Message: ${error.message}]`
+    );
+  }
+};
+
+export const getMintedAssets = async (policyId) => {
+  try {
+    const response = await cardano(`assets/policy/${policyId}`);
+
+    return response.map((asset) => asset.asset);
+  } catch (error) {
+    console.error(
+      `Unexpected error in getMintedAssets. [Message: ${error.message}]`
+    );
+  }
 };
 
 export const getTxDetails = async (hash) => {
-  const response = await cardano(`txs/${hash}/utxos`);
+  try {
+    const response = await cardano(`txs/${hash}/utxos`);
 
-  return {
-    hash: response.hash,
-    inputs: response.inputs,
-    outputs: response.outputs,
-  };
+    return {
+      hash: response.hash,
+      inputs: response.inputs,
+      outputs: response.outputs,
+    };
+  } catch (error) {
+    console.error(
+      `Unexpected error in getTxDetails. [Message: ${error.message}]`
+    );
+  }
 };
 
 export const getProtocolParameters = async () => {
-  const response = await cardano(`epochs/latest/parameters`);
+  try {
+    const response = await cardano(`epochs/latest/parameters`);
 
-  return {
-    linearFee: {
-      minFeeA: response.min_fee_a.toString(),
-      minFeeB: response.min_fee_b.toString(),
-    },
-    minUtxo: response.min_utxo,
-    poolDeposit: response.pool_deposit,
-    keyDeposit: response.key_deposit,
-    maxValSize: parseInt(response.max_val_size),
-    maxTxSize: parseInt(response.max_tx_size),
-    priceMem: parseFloat(response.price_mem),
-    priceStep: parseFloat(response.price_step),
-  };
+    return {
+      linearFee: {
+        minFeeA: response.min_fee_a.toString(),
+        minFeeB: response.min_fee_b.toString(),
+      },
+      minUtxo: response.min_utxo,
+      poolDeposit: response.pool_deposit,
+      keyDeposit: response.key_deposit,
+      maxValSize: parseInt(response.max_val_size),
+      maxTxSize: parseInt(response.max_tx_size),
+      priceMem: parseFloat(response.price_mem),
+      priceStep: parseFloat(response.price_step),
+    };
+  } catch (error) {
+    console.error(
+      `Unexpected error in getProtocolParameters. [Message: ${error.message}]`
+    );
+  }
 };
 
 const cardano = async (endpoint, headers, body) => {
