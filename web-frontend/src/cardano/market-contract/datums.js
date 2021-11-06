d7f6894ff93e115a3d893aa94a6ea71ecf30da50
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2021-11-06 12:26:28 +0100

    fix datum deserialization

--- web-frontend/src/cardano/market-contract/datums.js
+++ web-frontend/src/cardano/market-contract/datums.js
@@ -8,25 +8,29 @@ export const serialize = async ({ tn, cs, sellerAddress, price }) => {
   fields.add(Cardano.Instance.PlutusData.new_bytes(fromHex(tn)));
   fields.add(Cardano.Instance.PlutusData.new_bytes(fromHex(cs)));
   fields.add(Cardano.Instance.PlutusData.new_bytes(fromHex(sellerAddress)));
-  fields.add(Cardano.Instance.PlutusData.new_integer(Cardano.Instance.BigInt.from_str(price)));
+  fields.add(
+    Cardano.Instance.PlutusData.new_integer(
+      Cardano.Instance.BigInt.from_str(price)
+    )
+  );
 
   const datum = Cardano.Instance.PlutusData.new_constr_plutus_data(
-    Cardano.Instance.ConstrPlutusData.new(Cardano.Instance.Int.new_i32(0), fields)
+    Cardano.Instance.ConstrPlutusData.new(
+      Cardano.Instance.Int.new_i32(0),
+      fields
+    )
   );
 
   return datum;
 };
 
-export const deserialize = async (datum) => {
-  await Cardano.load();
+export const deserialize = (datum) => {
   const details = datum.as_constr_plutus_data().data();
 
   return {
     tn: toHex(details.get(0).as_bytes()),
     cs: toHex(details.get(1).as_bytes()),
-    sellerAddress: toHex(
-      Cardano.Instance.Ed25519KeyHash.from_bytes(details.get(2).as_bytes())
-    ),
-    price: details.get(3).as_integer().as_u64(),
+    sellerAddress: toHex(details.get(2).as_bytes()),
+    price: details.get(3).as_integer().to_str(),
   };
 };
