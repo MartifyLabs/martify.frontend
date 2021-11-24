23e2267e531daca7bec87314141d57391a15bf1f
Hong Jing (Jingles) <jinglescode@gmail.com>
2021-11-24 10:25:54 +0100

    remove headers

--- src/store/collection/api.js
+++ src/store/collection/api.js
@@ -180,10 +180,10 @@ export const asset_add_offer = (asset_id, price, callback) => async (dispatch) =
 export const opencnft_get_top_projects = (time, callback) => async (dispatch) => {
   fetch('https://api.opencnft.io/1/rank?window='+time, {
     method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
+    // headers: {
+    //   'Accept': 'application/json',
+    //   'Content-Type': 'application/json',
+    // },
   })
   .then(res => res.json())
   .then((res) => {
@@ -194,10 +194,10 @@ export const opencnft_get_top_projects = (time, callback) => async (dispatch) =>
 export const opencnft_get_policy = (policy_id, callback) => async (dispatch) => {
   fetch(`https://api.opencnft.io/1/policy/${policy_id}`, {
     method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
+    // headers: {
+    //   'Accept': 'application/json',
+    //   'Content-Type': 'application/json',
+    // },
   })
   .then(res => res.json())
   .then((res) => {
@@ -208,10 +208,10 @@ export const opencnft_get_policy = (policy_id, callback) => async (dispatch) => 
 export const opencnft_get_asset_tx = (asset_id, callback) => async (dispatch) => {
   fetch(`https://api.opencnft.io/1/asset/${asset_id}/tx`, {
     method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
+    // headers: {
+    //   'Accept': 'application/json',
+    //   'Content-Type': 'application/json',
+    // },
   })
   .then(res => res.json())
   .then((res) => {
