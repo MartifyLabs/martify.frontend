feebb7656b26f5d0e57c4f5648f0ef0db02364d2
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2021-11-26 16:24:34 +0100

    move configs to env

--- src/config.js
+++ src/config.js
@@ -1,20 +1,11 @@
-// TODO: store configuration in firebase
-
-/* production */
-// export const apiKey = "testnetAWfuCqaLZ5KtOSslc8ZTZyEWj36We8h7";
-// export const cardanoUrl = "https://cardano-testnet.blockfrost.io/api/v0/";
-// export const nami_network = 1; // this is mainnet
-
 /* development */
 export const api_host = "http://localhost:4000";
-// Blockfrost
-export const apiKey = "testnetAWfuCqaLZ5KtOSslc8ZTZyEWj36We8h7";
-export const cardanoUrl = "https://cardano-testnet.blockfrost.io/api/v0/";
-export const ipfsUrl = "https://ipfs.blockfrost.io/api/v0/";
-export const nami_network = 0; // this is testnet
+export const apiKey = process.env.REACT_APP_BLOCKFROST_API_KEY;
+export const cardanoUrl = process.env.REACT_APP_BLOCKFROST_API_URL;
+export const nami_network = process.env.REACT_APP_CARDANO_NETWORK_ID;
 
 export const urls = {
   root: "http://localhost:3000/",
-  cardanoscan: "https://testnet.cardanoscan.io/",
+  cardanoscan: process.env.REACT_APP_CARDANOSCAN_URL,
   ipfs: "https://infura-ipfs.io/ipfs/"
 }
