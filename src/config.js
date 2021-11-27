639b62a961f47b1e813ad1d37d9b5e51afed7bb8
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2021-11-27 15:27:53 +0100

    converst network id to int

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 export const api_host = "http://localhost:4000";
 export const apiKey = process.env.REACT_APP_BLOCKFROST_API_KEY;
 export const cardanoUrl = process.env.REACT_APP_BLOCKFROST_API_URL;
-export const nami_network = process.env.REACT_APP_CARDANO_NETWORK_ID;
+export const nami_network = parseInt(process.env.REACT_APP_CARDANO_NETWORK_ID);
 
 export const urls = {
   root: "http://localhost:3000/",
