5d4b2b574e63cfeb801c6b8ac45a2f29113cc381
Kazune Takeda <58710601+kazunetakeda25@users.noreply.github.com>
2021-12-09 00:58:55 +0900

    when price not set, show "not listed" instead

--- src/components/AssetCard/index.js
+++ src/components/AssetCard/index.js
@@ -6,70 +6,72 @@ import "./style.css";
 import AssetImageFigure from "../AssetImageFigure";
 import { fromLovelace } from "../../utils";
 
-const AssetCard = ({state_collection, asset, column_className, show_offer}) => {
+const AssetCard = ({ state_collection, asset, column_className, show_offer }) => {
 
   let collection = false;
-  if(asset){
+  if (asset) {
     collection = state_collection.policies_collections[asset.details.policyId]
   }
-  
+
   return (
     <>
-    {
-      asset ? (
-        <>
-        {
-          asset.details.onchainMetadata ? (
-            <div className={column_className?column_className:"column is-one-full-mobile is-half-tablet one-quarter-desktop is-one-quarter-widescreen is-one-quarter-fullhd"}>
-              <Link to={`/assets/${asset.details.policyId}/${asset.details.asset}`}>
-                <div className="card asset_card">
-                  <div className="card-image">
-                    <AssetImageFigure asset={asset}/>
-                  </div>
-                  <div className="card-content">
-                    <div className="media is-clipped">
-                      <div className="media-content clipped">
-                        <p className="title is-size-5 clipped">
-                          {asset.details.onchainMetadata.name}
-                        </p>
-                        <p className="subtitle is-size-7 clipped">
-                          {collection ? collection.is_martify_verified ? collection.meta.name : 
-                          collection.is_cnft_verified ? collection.is_cnft_verified :
-                          asset.details.policyId : asset.details.policyId
-                          }
-                        </p>
-                        {
-                          asset.status.locked ? 
-                          <p className="title is-size-5 tag-price">
-                            <p>{fromLovelace(asset.status.datum.price)}<span className="ada_symbol">&nbsp;₳</span></p>
-                          </p> : <></>
-                        }
-                        {
-                          show_offer ? asset.offers ? Object.keys(asset.offers).length ? (
-                            <span className="tag is-warning is-medium is-rounded price_tag" style={{top:"50px"}}>
-                              <p className="" data-tooltip={`Highest offer`}>
-                                <span className="ada_symbol">₳</span>
-                                {
-                                  Math.max.apply(Math, Object.keys(asset.offers).map(function(key){
-                                    return asset.offers[key];
-                                  }).map(function(o) { return o.p; }))
-                                }
-                              </p>
-                            </span>
-                          ) : <></> : <></> : <></>
-                        }
-                        
+      {
+        asset ? (
+          <>
+            {
+              asset.details.onchainMetadata ? (
+                <div className={column_className ? column_className : "column is-one-full-mobile is-half-tablet one-quarter-desktop is-one-quarter-widescreen is-one-quarter-fullhd"}>
+                  <Link to={`/assets/${asset.details.policyId}/${asset.details.asset}`}>
+                    <div className="card asset_card">
+                      <div className="card-image">
+                        <AssetImageFigure asset={asset} />
+                      </div>
+                      <div className="card-content">
+                        <div className="media is-clipped">
+                          <div className="media-content clipped">
+                            <p className="title is-size-5 clipped">
+                              {asset.details.onchainMetadata.name}
+                            </p>
+                            <p className="subtitle is-size-7 clipped">
+                              {collection ? collection.is_martify_verified ? collection.meta.name :
+                                collection.is_cnft_verified ? collection.is_cnft_verified :
+                                  asset.details.policyId : asset.details.policyId
+                              }
+                            </p>
+                            {
+                              asset.status.locked ?
+                                <p className="title is-size-5 tag-price">
+                                  <p>{fromLovelace(asset.status.datum.price)}<span className="ada_symbol">&nbsp;₳</span></p>
+                                </p> : <p className="title is-size-5 tag-price">
+                                  <p><span className="ada_symbol">Not Listed</span></p>
+                                </p>
+                            }
+                            {
+                              show_offer ? asset.offers ? Object.keys(asset.offers).length ? (
+                                <span className="tag is-warning is-medium is-rounded price_tag" style={{ top: "50px" }}>
+                                  <p className="" data-tooltip={`Highest offer`}>
+                                    <span className="ada_symbol">₳</span>
+                                    {
+                                      Math.max.apply(Math, Object.keys(asset.offers).map(function (key) {
+                                        return asset.offers[key];
+                                      }).map(function (o) { return o.p; }))
+                                    }
+                                  </p>
+                                </span>
+                              ) : <></> : <></> : <></>
+                            }
+
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  </Link>
                 </div>
-              </Link>
-            </div>
-          ) : <></>
-        }
-        </>
-      ) : <></>
-    }
+              ) : <></>
+            }
+          </>
+        ) : <></>
+      }
     </>
   )
 };
