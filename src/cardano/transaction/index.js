603651ee22de914f33ec7f12a321c2c50a777d84
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2021-12-08 12:44:38 +0100

    load validator based on network id

--- src/cardano/transaction/index.js
+++ src/cardano/transaction/index.js
@@ -313,19 +313,33 @@ export const valueToAssets = (value) => {
 };
 
 const getProtocolParameters = () => {
-  return {
-    linearFee: {
-      minFeeA: "44",
-      minFeeB: "155381",
-    },
-    minUtxo: "34482",
-    poolDeposit: "500000000",
-    keyDeposit: "2000000",
-    maxValSize: 5000,
-    maxTxSize: 16384,
-    priceMem: 0.0577,
-    priceStep: 0.0000721,
-  };
+  return parseInt(process.env.REACT_APP_CARDANO_NETWORK_ID) === 0
+    ? {
+        linearFee: {
+          minFeeA: "44",
+          minFeeB: "155381",
+        },
+        minUtxo: "34482",
+        poolDeposit: "500000000",
+        keyDeposit: "2000000",
+        maxValSize: 5000,
+        maxTxSize: 16384,
+        priceMem: 0.0577,
+        priceStep: 0.0000721,
+      }
+    : {
+        linearFee: {
+          minFeeA: "44",
+          minFeeB: "155381",
+        },
+        minUtxo: "34482",
+        poolDeposit: "500000000",
+        keyDeposit: "2000000",
+        maxValSize: 5000,
+        maxTxSize: 16384,
+        priceMem: 0.0577,
+        priceStep: 0.0000721,
+      };
 };
 
 const setCollateral = (txBuilder, utxos) => {
--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 /* development */
 export const apiKey = process.env.REACT_APP_BLOCKFROST_API_KEY;
 export const cardanoUrl = process.env.REACT_APP_BLOCKFROST_API_URL;
-export const nami_network = parseInt(process.env.REACT_APP_CARDANO_NETWORK_ID);
 
 export const urls = {
   root: "http://localhost:3000/",
